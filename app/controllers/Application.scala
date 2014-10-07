da019f5eabb6b2e3b6921b6b3442d30ed64e718a
Francis Tseng <f+accounts@frnsys.com>
2014-10-07 12:02:11 -0400

    join mobs via form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,10 +7,34 @@ import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.concurrent.Execution.Implicits._ 
 
+import play.api.data._
+import play.api.data.Forms._
+
+case class MobData(name: String, username: String)
+
 object Application extends Controller {
 
+  val mobForm = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "username" -> nonEmptyText
+    )(MobData.apply)(MobData.unapply)
+  )
+
+
   def index = Action { implicit request =>
-    Ok(views.html.index())
+    Ok(views.html.index(mobForm))
+  }
+
+  def joinMob = Action { implicit request =>
+    mobForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.index(formWithErrors))
+      },
+      mobData => {
+        Redirect(routes.Application.mob(mobData.name, Option(mobData.username)))
+      }
+    )
   }
 
   def mob(slug: String, username: Option[String]) = Action { implicit request =>
